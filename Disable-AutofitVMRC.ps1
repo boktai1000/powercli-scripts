f80adbf6b23df66965f2d5434f27a500358f7faf
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-16 14:44:36 -0500

    Update Disable-AutofitVMRC.ps1

--- Disable-AutofitVMRC.ps1
+++ Disable-AutofitVMRC.ps1
@@ -1,3 +1,7 @@
+# https://www.vgemba.net/vmware/powercli/VMConsole-Resolution-Change/
+# https://serverfault.com/questions/830849/esxi-6-5-how-to-turn-off-autofit-window-automatic-display-resolution-change
+# https://kb.vmware.com/s/article/52031
+
 Connect-VIServer
 $vm = Read-Host -Prompt 'Enter a VM name to edit'
 Get-VM -Name $vm | New-AdvancedSetting -Name guestInfo.svga.wddm.modeset -Value FALSE -Confirm:$false
